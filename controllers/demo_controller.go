a8f8ab48a03b1bdba73700b36f88b03c307b167c
root <root@apaas8030.i1fjeftrltcetglym4fv3msarh.cx.internal.chinacloudapp.cn>
2021-03-24 15:19:59 +0000

    init operator

---
+++ controllers/demo_controller.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	apaasenvisioncncomv1beta1 "git.envisioncn.com/apaas/demo/api/v1beta1"
+)
+
+// DemoReconciler reconciles a Demo object
+type DemoReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=apaas.envisioncn.com.apaas.envisioncn.com,resources=demoes,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apaas.envisioncn.com.apaas.envisioncn.com,resources=demoes/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apaas.envisioncn.com.apaas.envisioncn.com,resources=demoes/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Demo object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
+func (r *DemoReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := r.Log.WithValues("demo", req.NamespacedName)
+
+	// your logic here
+	demo := &apaasenvisioncncomv1beta1.Demo{}
+	err := r.Get(ctx, req.NamespacedName, demo)
+	if nil != err {
+		if errors.IsNotFound(err) {
+			log.Info("Demo resource not found. Ignoring since object must be deleted", "Demo.NamespacedName", req.NamespacedName.String())
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+	log.Info("Demo info.", "Demo.APIVersion", "Demo.NamespacedName", req.NamespacedName.String(), demo.APIVersion,
+		"Demo.Host", demo.Spec.Host, "Demo.Port", demo.Spec.Port)
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *DemoReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&apaasenvisioncncomv1beta1.Demo{}).
+		Complete(r)
+}
